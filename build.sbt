2478e2063d7d3b3f6000f079c7889573149e7a98
= <=>
2020-12-08 16:34:24 +0100

    lab elections

--- build.sbt
+++ build.sbt
@@ -9,3 +9,6 @@ libraryDependencies += "org.apache.spark" %% "spark-core" % "3.0.1"
 
 // https://mvnrepository.com/artifact/org.apache.spark/spark-core
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.0.1"
+
+// https://mvnrepository.com/artifact/com.oracle/ojdbc6
+libraryDependencies += "com.oracle" % "ojdbc6" % "12.1.0.1-atlassian-hosted"
--- src/main/scala/Election.scala
+++ src/main/scala/Election.scala
@@ -1,4 +1,5 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, lit, repeat}
 
 object Election extends App {
   val spark = SparkSession
@@ -7,12 +8,62 @@ object Election extends App {
     .config("spark.master", "local")
     .getOrCreate()
 
-  val df_biden = spark.read
+  var df_biden = spark.read
+    .format("csv")
     .option("header", "true")
-    .load("biden.csv")
+    .option("inferSchema", "true")
+    .option("readmode", "permissive")
+    .load("C:\\Users\\yelad\\IdeaProjects\\spark-elections\\data\\hashtag_joebiden.csv")
 
-  val df_trump = spark.read
+
+  
+  var df_trump = spark.read
+    .format("csv")
     .option("header", "true")
-    .load("trump.csv")
+    .option("inferSchema", "true")
+    .load("C:\\Users\\yelad\\IdeaProjects\\spark-elections\\data\\hashtag_donaldtrump.csv")
+
+  df_biden.printSchema()
+  df_trump.printSchema()
+
+
+
+  // Afin de faire la fusion des deux dataframe et les distinguer par la suite, on ajoute la colonne candidate
+
+  df_biden = df_biden.withColumn("candidate", lit("biden"))
+    .select("created_at", "tweet_id", "likes", "retweet_count", "city", "country", "state", "candidate")
+  df_trump = df_trump.withColumn("candidate", lit("trump"))
+    .select("created_at", "tweet_id", "likes", "retweet_count", "city", "country", "state", "candidate")
+
+  df_biden.show()
+  df_trump.show()
+
+  // Jointure des deux dataframe
+
+  var df = df_biden.union(df_trump)
+
+  df.show()
+
+  println(df.count())
+
+  // Cleansing
+
+  df = df.dropDuplicates("tweet_id")
+
+  println(df.count())
+
+  df.na.drop("all")
+
+  // Wrangling : TODO
+
+  df.select("candidate", "likes")
+
+  // Stocker le résultat final
+  // Parquet ==> Twitter est un format de sérialisation qui contient à l'intérieur le schéma des données
+  // Orc ==> Facebook
+  // ProtoBuff ==> Google
+  // Avro
+  df.write
+    .parquet("C:\\Users\\yelad\\IdeaProjects\\spark-elections\\output")
 
 }
